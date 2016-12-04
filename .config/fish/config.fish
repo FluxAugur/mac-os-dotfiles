dc41a1da01107cdaf6bac739116b7b295df5d303
Nathanial L. McConnell <fluxaugur@gmail.com>
2016-12-04 16:18:04 -0500

    Added SASS/SCSS lint configs. Added RBENV root var.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,5 @@
 set fish_greeting
 
-# set -gx LC_ALL en_US.UTF-8
-# set -gx LC_CTYPE en_US.UTF-8
-
-# fish will show a prompt, don't mess with my theme
-# set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
-
-# don't allow global pip by accident when no virtualenv is sourced
-# set -gx PIP_REQUIRE_VIRTUALENV true
-
 # allow global pip if necessary when no virtualenv is sourced
 function gpip
     begin
@@ -20,22 +11,8 @@ end
 # hacks to use XDG config location for easier backup
 alias tmux "tmux -2f $HOME/.config/tmux/config"
 
-# less, don't save any history
-# set -gx LESSHISTFILE /dev/null
-
-# set -gx DOTFILES "$HOME/.local/share/dotfiles/"
-
 # https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
 alias dot "git --git-dir=$DOTFILES --work-tree=$HOME"
-
-# homebrew
-# set -gx HOMEBREW_CASK_OPTS "--appdir=$HOME/Applications"
-
-# base directories
-#   https://specifications.freedesktop.org/basedir-spec/basedir-spec-0.6.html
-# set -gx XDG_DATA_HOME "$HOME/.local/share"
-# set -gx XDG_CONFIG_HOME "$HOME/.config"
-# set -gx XDG_CACHE_HOME "$HOME/.cache"
 
 # add user binaries to path
 set -gx PATH "$HOME/.local/share/bin/" $PATH
@@ -91,3 +68,4 @@ set -g theme_color_scheme terminal-dark
 # set -g __color_username              434C5E 88C0D0
 # set -g __color_rvm                   D08770 ECEFF4 --bold
 # set -g __color_virtualfish           5E81AC ECEFF4 --bold
+status --is-interactive; and source (rbenv init -|psub)
