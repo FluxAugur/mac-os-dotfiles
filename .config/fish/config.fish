ae7693cfb15907681f1fbe7faf7688c28328f5e2
Nathanial L. McConnell <fluxaugur@gmail.com>
2016-12-04 09:45:23 -0500

    Add paths. Update fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,13 @@
 set fish_greeting
 
-set -gx LC_ALL en_GB.UTF-8
-set -gx LC_CTYPE en_GB.UTF-8
+# set -gx LC_ALL en_US.UTF-8
+# set -gx LC_CTYPE en_US.UTF-8
 
 # fish will show a prompt, don't mess with my theme
-set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
+# set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
 
 # don't allow global pip by accident when no virtualenv is sourced
-set -gx PIP_REQUIRE_VIRTUALENV true
+# set -gx PIP_REQUIRE_VIRTUALENV true
 
 # allow global pip if necessary when no virtualenv is sourced
 function gpip
@@ -21,24 +21,21 @@ end
 alias tmux "tmux -2f $HOME/.config/tmux/config"
 
 # less, don't save any history
-set -gx LESSHISTFILE /dev/null
+# set -gx LESSHISTFILE /dev/null
 
-set -gx DOTFILES "$HOME/.local/share/dotfiles/"
+# set -gx DOTFILES "$HOME/.local/share/dotfiles/"
 
 # https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
 alias dot "git --git-dir=$DOTFILES --work-tree=$HOME"
 
 # homebrew
-set -gx HOMEBREW_CASK_OPTS "--appdir=$HOME/Applications"
+# set -gx HOMEBREW_CASK_OPTS "--appdir=$HOME/Applications"
 
 # base directories
 #   https://specifications.freedesktop.org/basedir-spec/basedir-spec-0.6.html
-set -gx XDG_DATA_HOME "$HOME/.local/share"
-set -gx data_home $XDG_DATA_HOME
-set -gx XDG_CONFIG_HOME "$HOME/.config"
-set -gx config_home $XDG_CONFIG_HOME
-set -gx XDG_CACHE_HOME "$HOME/.cache"
-set -gx cache_home $XDG_CACHE_HOME
+# set -gx XDG_DATA_HOME "$HOME/.local/share"
+# set -gx XDG_CONFIG_HOME "$HOME/.config"
+# set -gx XDG_CACHE_HOME "$HOME/.cache"
 
 # add user binaries to path
 set -gx PATH "$HOME/.local/share/bin/" $PATH
