cfc57a8c404f6316ccfb5a505b66e8ad80df1c77
Nathanial L. McConnell <fluxaugur@gmail.com>
2016-12-04 10:27:11 -0500

    Update fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -41,4 +41,52 @@ alias dot "git --git-dir=$DOTFILES --work-tree=$HOME"
 set -gx PATH "$HOME/.local/share/bin/" $PATH
 
 # config bobthefish
+#   https://github.com/oh-my-fish/theme-bobthefish
+set -g theme_display_git yes
+set -g theme_display_git_untracked yes
+set -g theme_display_git_ahead_verbose yes
+set -g theme_git_worktree_support yes
+set -g theme_display_vagrant yes
+set -g theme_display_docker_machine yes
+set -g theme_display_hg yes
+set -g theme_display_virtualenv yes
+set -g theme_display_ruby yes
+set -g theme_display_user yes
+set -g theme_display_vi yes
+set -g theme_display_date yes
+set -g theme_display_cmd_duration yes
+set -g theme_title_display_process yes
+set -g theme_title_display_path yes
+set -g theme_title_use_abbreviated_path yes
+set -g theme_date_format "+%a %H:%M"
+set -g theme_avoid_ambiguous_glyphs yes
+set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+set -g default_user lastson
+
+set -g theme_color_scheme user
+set -l colorfg 2E3440
+
+set -g __color_initial_segment_exit  2E3440 ce000f --bold
+set -g __color_initial_segment_su    3B4252 189303 --bold
+set -g __color_initial_segment_jobs  ECEFF4 255e87 --bold
+
+set -g __color_path                  333333 999999
+set -g __color_path_basename         333333 ffffff --bold
+set -g __color_path_nowrite          660000 cc9999
+set -g __color_path_nowrite_basename 660000 cc9999 --bold
+
+set -g __color_repo                  addc10 0c4801
+set -g __color_repo_work_tree        addc10 ffffff --bold
+set -g __color_repo_dirty            ce000f ffffff
+set -g __color_repo_staged           f6b117 3a2a03
+
+set -g __color_vi_mode_default       999999 333333 --bold
+set -g __color_vi_mode_insert        189303 333333 --bold
+set -g __color_vi_mode_visual        f6b117 3a2a03 --bold
+
+set -g __color_vagrant               48b4fb ffffff --bold
+set -g __color_username              cccccc 255e87
+set -g __color_rvm                   af0000 cccccc --bold
+set -g __color_virtualfish           005faf cccccc --bold
