458927243edda3006cc704ef5d279e73580cd489
Nathanial L. McConnell <fluxaugur@gmail.com>
2016-12-08 01:46:46 -0500

    Update fish functions to update stuff.

--- .config/fish/functions/update-all.fish
+++ .config/fish/functions/update-all.fish
@@ -1,14 +1,14 @@
 function update-all -d "Update macOS software, Homebrew brews & casks, gems, packages (apms, npms), repos"
   # macOS software updates
-  command update-os
+  update-os
   # Homebrew brews updates
-  command update-brews
+  update-brews
   # Homebrew cask updates
-  command update-casks
+  update-casks
   # NPM updates
-  command update-npms
+  update-npms
   # Gem updates
-  command update-gems
+  update-gems
   # APM updates
-  command update-apms
+  update-apms
 end
