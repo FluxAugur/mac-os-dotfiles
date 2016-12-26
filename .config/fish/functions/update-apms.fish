64ae40955e085906b91b7bd3a0d082d8ea8f9d30
Nathanial L. McConnell <fluxaugur@gmail.com>
2016-12-26 12:35:18 -0500

    Update brews. Remove apm upgrade confirmation. Update macOS defaults.

--- .config/fish/functions/update-apms.fish
+++ .config/fish/functions/update-apms.fish
@@ -1,3 +1,3 @@
 function update-apms -d "Update, upgrade, dedupe, and clean Atom apms"
-  command apm update; and apm upgrade; and apm dedupe; and apm clean
+  command apm update; and apm upgrade --confirm false; and apm dedupe; and apm clean
 end
