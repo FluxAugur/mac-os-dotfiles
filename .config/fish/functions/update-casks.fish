95e5a004fae3bef99008194eb8148b183c5c2f8e
Nathanial L. McConnell <fluxaugur@gmail.com>
2016-12-27 06:23:58 -0500

    Remove duplicates in `Open With` menu.

--- .config/fish/functions/update-casks.fish
+++ .config/fish/functions/update-casks.fish
@@ -1,3 +1,5 @@
 function update-casks -d "Update, upgrade, and cleanup Homebrew casks"
   command brew cask update; and brew cu; and brew cask cleanup; and brew cask doctor
+  # Remove duplicates in the “Open With” menu
+  /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
 end
